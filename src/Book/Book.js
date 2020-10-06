bd44bf66fe3a3061ea9ab120908e77be772e1e42
Andre <afowlk1@gmail.com>
2020-10-06 14:46:23 -0400

    90% working! :)

--- src/Book/Book.js
+++ src/Book/Book.js
@@ -4,14 +4,17 @@ import './Book.css'
 export default function Book(props) {
     return (
         <div className="book">
+            {console.log(props)}
             <ul>
                 {props.book.map(book => (
-                    <li>
-                        <h2>{book.title}</h2>
-                        <p>Author: {book.author}</p>
-                        <p>Price: {book.price}</p>
+                    <li key={book.etag}> {/*Used google defined etag as unique id for li */}
+                        <h2>{book.volumeInfo.title}</h2>
+                        <p>Author: {book.volumeInfo.authors}</p>
+                        {/* Need to fix line below - Only works with filter for paid books */}
+                        {/* <p>Retail Price: {book.saleInfo.retailPrice.amount}</p>  */}
                         <p>{book.snippet}</p>
-                        <img src={book.thumbnail} />
+                        {/* <a href= {book.selfLink}>Link:</a> */}
+                        <img src={book.volumeInfo.imageLinks.thumbnail} />
                     </li>
                 ))
                 }
