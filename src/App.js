54f67c337c28a5dfffe40fde316f497f5ef56921
Andre <afowlk1@gmail.com>
2020-10-06 14:10:02 -0400

    Immediately post 10/5 Mario meeting - Need to make ??? more updates to complete

--- src/App.js
+++ src/App.js
@@ -3,29 +3,76 @@ import './App.css';
 import BookList from './BookList/BookList'
 import SearchBar from './SearchBar/SearchBar'
 
-const books =[
-  {
-    title:"Night Train to Sugar Hill",
-    author:"Iceberg Slim",
-    price:"$29.99",
-    snippet:"A thrilling jaunt through life in 1950's Detroit",
-    thumbnail: "https://images-na.ssl-images-amazon.com/images/I/51Qhi4p1tjL._SX309_BO1,204,203,200_.jpg"
-  },
-  {
-    title:"A Walk in the Woods",
-    author:"Bill Bryson",
-    price:"$8.99",
-    snippet:"A tale of life, journey, and exploration along the Appalachian trail",
-    thumbnail: "https://images-na.ssl-images-amazon.com/images/I/61ksRYf1yHL._SX302_BO1,204,203,200_.jpg"
-  }
-]
+// const books = [
+//   {
+//     title: "Night Train to Sugar Hill",
+//     author: "Iceberg Slim",
+//     price: "$29.99",
+//     snippet: "A thrilling jaunt through life in 1950's Detroit",
+//     thumbnail: "https://images-na.ssl-images-amazon.com/images/I/51Qhi4p1tjL._SX309_BO1,204,203,200_.jpg"
+//   },
+//   {
+//     title: "A Walk in the Woods",
+//     author: "Bill Bryson",
+//     price: "$8.99",
+//     snippet: "A tale of life, journey, and exploration along the Appalachian trail",
+//     thumbnail: "https://images-na.ssl-images-amazon.com/images/I/61ksRYf1yHL._SX302_BO1,204,203,200_.jpg"
+//   }
+// ]
 
 export default class App extends Component {
+  state = {
+    books: []
+  }
+
+  handleSubmit=(e)=> {
+    e.preventDefault()
+    const searchTerm = e.target.searchTerm.value
+    const printType = e.target.printType.value
+    const bookType = e.target.bookType.value
+    const url = "https://www.googleapis.com/books/v1/volumes"
+    const params = {
+      key: "AIzaSyByTnTvg1woG78Azn1NKIbcLaleyJj8t8Y",
+      q: searchTerm
+    }
+
+    if (printType) {
+      params.printType = printType
+    }
+
+    if (bookType) {
+      params.filter = bookType
+    }
+
+    const querystring = Object.keys(params).map(k => `${k}=${params[k]}`).join("&")
+
+    fetch(url + "?" + querystring)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('Something went wrong, please try again')
+        }
+        return res
+      })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          books: data.items
+        })
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        })
+      })
+  }
+
   render() {
     return (
       <div className="App">
-        <SearchBar />
-        <BookList books ={books} />
+        <SearchBar
+          handleSubmit={this.handleSubmit}
+        />
+        <BookList books={this.state.books} />
       </div>
     )
   }
--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import './SearchBar.css'
 
-export default function SearchBar() {
+export default function SearchBar(props) {
     return (
         <div>
             <h1>Google Book Search</h1>
 
-            <form className="book__search">
+            <form className="book__search" onSubmit={props.handleSubmit}>
                 <label htmlFor="searchTerm"> Search: </label>
-                <input type="text" name="searchTerm" id="searchTerm" placeholder="The Alchemist" />
+                <input type="text" name="searchTerm" id="searchTerm" placeholder="The Alchemist" required/>
                 <button type="submit">Search</button>
                 <br />
                 <label htmlFor="printType"> Print Type: </label>
-                <select value="printType"> {/*Need to complete! */}
+                <select name="printType" id="printType"> 
                     <option value="all">All</option>
                     <option value="books">Books</option>
                     <option value="magazines">Magazines</option>
                 </select> 
                 <label htmlFor="bookType"> Book Type: </label>
-                <select value="bookType"> {/*Need to complete! */}
+                <select name="bookType" id="bookType">
                     <option value="ebooks">All Google eBooks</option>
                     <option value="free-ebooks">Google eBooks with full volume viewability</option>
                     <option value="full">Public can view entire volume text</option>
